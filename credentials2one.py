d1dc9e4ed83dddc49402738fcb3a21bc361881fe
日后定负责 <matrix273@gmail.com>
2021-01-05 12:07:12 +0800

    Update credentials2one.py
    添加支持长宽切换

--- credentials2one.py
+++ credentials2one.py
@@ -31,18 +31,24 @@ def crop(pic):
 #             cv2.imwrite("front_canvas.png", canvas)
             width,height=int(w),int(h) #转换宽,高为整形
             src_pts = rectCnt.astype("float32")
-            # coordinate of the points in box points after the rectangle has been
-            # straightened
-            dst_pts = np.array([[0, height-1],
-                                [0, 0],
-                                [width-1, 0],
-                                [width-1, height-1]], dtype="float32")
-
-            # the perspective transformation matrix
-            M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-
-            # directly warp the rotated rectangle to get the straightened rectangle
-            croped = cv2.warpPerspective(img, M, (width, height))
+            if w > h:
+                dst_pts = np.array([[0, height-1],
+                                    [0, 0],
+                                    [width-1, 0],
+                                    [width-1, height-1]], dtype="float32")
+                # the perspective transformation matrix
+                M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+                # directly warp the rotated rectangle to get the straightened rectangle
+                croped = cv2.warpPerspective(img, M, (width, height))
+            elif w < h:
+                dst_pts = np.array([[height-1, width-1],
+                                    [0, width -1],
+                                    [0, 0],
+                                    [height-1, 0]], dtype="float32")
+                # the perspective transformation matrix
+                M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+                # directly warp the rotated rectangle to get the straightened rectangle
+                croped = cv2.warpPerspective(img, M, (height,width))
     return croped
 
 def warp(imread,bias=0): # 定义覆盖函数
